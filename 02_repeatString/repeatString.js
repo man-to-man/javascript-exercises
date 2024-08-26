fbac18fcd66ec5c3b9d718ef04ce66e9360373ef
man-to-man <tamirhadani@gmail.com>
2024-08-26 20:37:53 +0300

    done 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,iter) {
+    if(iter<0){
+        return 'ERROR';
+    }else{
+        out_str="";
+    for(let i=0;i<iter;i++){
+        out_str+=str; 
+    }
+    return out_str;
+    }
 };
-
+console.log(repeatString('hey',3));
 // Do not edit below this line
 module.exports = repeatString;
