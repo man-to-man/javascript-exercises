0aa49173497caca7c00c215b1ab9dec949d0b9c1
man-to-man <tamirhadani@gmail.com>
2024-09-04 16:32:57 +0300

    the function does not invert the string at all more dubbaging needed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,25 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let temp,bottomIndex=0,topIndex=str.length-1;
+    if (str.length % 2 == 0){
+        for(let i=0;i<(str.length)/2;i++){
+            console.log(str);
+            temp=str[bottomIndex];
+            str[bottomIndex]=str[topIndex];
+            str[topIndex]=temp;
+            bottomIndex+=1;
+            topIndex-=1;
+        };return str;
+    }else{
+        for(let i=0;i<(str.length-1)/2;i++){
+            console.log(str);
+            temp=str[bottomIndex];
+            str[bottomIndex]=str[topIndex];
+            str[topIndex]=temp;
+            bottomIndex+=1;
+            topIndex-=1;
+        };return str;
+    }
+    
 };
 
 // Do not edit below this line
