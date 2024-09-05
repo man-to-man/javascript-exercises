15163e07eb53434fee9849c824acf36758c404b1
man-to-man <tamirhadani@gmail.com>
2024-09-05 18:20:53 +0300

    tried to count the amount of digits before the floating point with dividing them seperitly. i need to run the while loop with a copyed value to a trunc of 0 insted and count like that.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const initCalc=(F-32)*(5/9);
+  let divider=1;
+  let cnt=0;
+  while(Math.trunc(initCalc) % divider===0){
+    console.log(cnt);
+    divider*=10;
+    cnt+=1;
+    console.log(cnt);
+    
+  }
+  return parseFloat(initCalc.toPrecision(cnt));
 };
+console.log(convertToCelsius(100));
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  const initCalc = C*(9/5)+32;
+  let divider=1;
+  let cnt=0;
+  while(Math.trunc(initCalc) % divider===0){
+    divider*=10;
+    cnt+=1;
+  }
+  return parseFloat(initCalc.toPrecision(cnt));
 };
 
 // Do not edit below this line
