aeb624b45c09dd7dc787629931dc5ecbefd2c397
man-to-man <tamirhadani@gmail.com>
2024-09-05 13:24:07 +0300

    done, learned about rest arguments, arguments in general and the includes() array method.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toErase) {
+    newArr=[];
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<toErase.length; j++){
+            if((toErase.includes(arr[i])===false)&&(newArr.includes(arr[i])===false)){
+                newArr.push(arr[i]);
+            };
+        };
+    }
+    return newArr;
 };
-
+console.log(removeFromArray([1,2,3,4],3,2))
 // Do not edit below this line
 module.exports = removeFromArray;
