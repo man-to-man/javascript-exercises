50dfc8ab0c89fb66478c16b7e195661615f884a3
man-to-man <tamirhadani@gmail.com>
2024-09-05 15:14:18 +0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let ans=0;
+    let operand=start;
+    if(Number.isInteger(start) && Number.isInteger(end) && start>0 && end>0){
+        if(start>end){
+            
+            while(operand>=end){
+                ans+=operand;
+                operand-=1;
+            }
+        }else{
+            while(operand<=end){
+                ans+=operand;
+                operand+=1;
+            }
+        }return ans;
+    }else{
+        return 'ERROR';
+    }
 
+    
 };
-
+console.log(sumAll(-10,4));
 // Do not edit below this line
 module.exports = sumAll;
