cdd2817acae91bb17e535aa7c71630a423b932c4
man-to-man <tamirhadani@gmail.com>
2024-09-06 18:52:29 +0300

    done and done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -6,9 +6,6 @@ const palindromes = function (str) {
         }
     }
     let n=format.length;
-    console.log(format);
-    console.log(format.slice(0,Math.floor(n/2)+1));
-    console.log(format.slice(Math.floor(n/2),n).reverse());
     if(n%2!==0){
         if(format.slice(0,Math.floor(n/2)+1).join('')
             ===
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n=parseInt(n);
+    if(n>=0){
+        let sum=0;
+        let s=[0,1];
+        for(let i=1;i<=n-1;i++){
+            console.log(s);
+            sum+=s[i-1]+s[i];
+            s.push(sum)
+            sum=0;
+        }
+        if(n!==0){
+            return s[s.length-1];
+        }else{
+            return s[0];
+        }
+            
+    }else{
+        return 'OOPS';
+    }
+    
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
