ee89dbecdb10626edcb38e58a7f4fbb5007381dc
man-to-man <tamirhadani@gmail.com>
2024-09-06 17:52:35 +0300

    done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    let format=[];
+    for(const ch of str){
+        if(ch.match(/^[a-zA-Z0-9]+$/g)){
+            format.push(ch.toLowerCase());
+        }
+    }
+    let n=format.length;
+    console.log(format);
+    console.log(format.slice(0,Math.floor(n/2)+1));
+    console.log(format.slice(Math.floor(n/2),n).reverse());
+    if(n%2!==0){
+        if(format.slice(0,Math.floor(n/2)+1).join('')
+            ===
+        format.slice(Math.floor(n/2),n).reverse().join('')){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        if(format.slice(0,(n/2)).join('')
+            ===
+        format.slice((n/2),n).reverse().join('')){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+console.log(palindromes('r3ace3car'));
 // Do not edit below this line
 module.exports = palindromes;
