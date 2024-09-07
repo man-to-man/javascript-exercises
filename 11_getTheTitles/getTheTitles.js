cd8045a5f7a6e38416793a389d40850891eded14
man-to-man <tamirhadani@gmail.com>
2024-09-07 15:19:28 +0300

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    arr=[]
+    for(let i=0; i<books.length;i++){
+        arr.push(books[i].title);
+    }return arr;
 };
 
 // Do not edit below this line
