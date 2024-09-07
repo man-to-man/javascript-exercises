6c3e26b737b28faaf5009157f496985c6538a674
man-to-man <tamirhadani@gmail.com>
2024-09-07 16:31:55 +0300

    done!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let difArray=[];
+    let birthYears=[];
+    for(let i=0;i<people.length;i++){
+        difArray.push(people[i].yearOfDeath-people[i].yearOfBirth);
+        birthYears.push(people[i].yearOfBirth);
+    }
+    for(let j=0;j<difArray.length;j++){
+        if(Number.isNaN(difArray[j])){
+            difArray[j]=1;
+        }
+    }
+    if(difArray.includes(1)==true){
+        if(birthYears[difArray.indexOf(1)]==Math.min.apply(null,birthYears)){
+            return people[difArray.indexOf(1)];
+        }else{
+            return people[difArray.indexOf(Math.max.apply(null,difArray))];
+        }
+    }else{
+        return people[difArray.indexOf(Math.max.apply(null,difArray))];
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
